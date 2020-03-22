cf5581356a84493aafcf0266d3e40dad5879dbe5
Nick <legokol3@gmail.com>
2020-03-22 23:40:42 +0300

    Vector3D created and finished.

--- Vector3D.cpp
+++ Vector3D.cpp
@@ -1,1 +1,92 @@
+#include <iostream>
+#include <cmath>
 #include "Vector3D.h"
+
+using namespace std;
+
+double Vector3D::getX() const {
+    return x;
+}
+
+double Vector3D::getY() const {
+    return y;
+}
+
+double Vector3D::getZ() const {
+    return z;
+}
+
+void Vector3D::setX(double _x) {
+    x = _x;
+}
+
+void Vector3D::setY(double _y) {
+    y = _y;
+}
+
+void Vector3D::setZ(double _z) {
+    z = _z;
+}
+
+void Vector3D::rotateX(double angle) {
+    angle = angle * 3.14 / 180;
+    double _y = getY(), _z = getZ();
+    setY(_y * cos(angle) + _z * sin(angle));
+    setZ(-_y * sin(angle) + _z * cos(angle));
+}
+
+void Vector3D::rotateY(double angle) {
+    angle = angle * 3.14 / 180;
+    double _z = getZ(), _x = getX();
+    setZ(_z * cos(angle) + _x * sin(angle));
+    setX(-_z * sin(angle) + _x * cos(angle));
+}
+
+void Vector3D::rotateZ(double angle) {
+    angle = angle * 3.14 / 180;
+    double _x = getX(), _y = getY();
+    setX(_x * cos(angle) + _y * sin(angle));
+    setY(-_x * sin(angle) + _y * cos(angle));
+}
+
+bool Vector3D::operator==(const Vector3D &v2) const {
+    return x == v2.getX() && y == v2.getY();
+}
+
+bool Vector3D::operator!=(const Vector3D &v2) const {
+    return !(*this == v2);
+}
+
+Vector3D Vector3D::operator+(const Vector3D &v2) const {
+    return Vector3D(getX() + v2.getX(), getY() + v2.getY(), getZ() + v2.getZ());
+}
+
+Vector3D Vector3D::operator-(const Vector3D &v2) const {
+    return Vector3D(getX() - v2.getX(), getY() - v2.getY(), getZ() - v2.getZ());
+}
+
+Vector3D Vector3D::operator*(const double a) const {
+    return Vector3D(getX() * a, getY() * a, getZ() * a);
+}
+
+double Vector3D::operator*(const Vector3D &v2) const {
+    return getX() * v2.getX() + getY() * v2.getY() + getZ() * v2.getZ();
+}
+
+Vector3D operator*(double a, const Vector3D &v) {
+    return v * a;
+}
+
+ostream &operator<<(ostream &os, const Vector3D &v) {
+    os << '(' << v.getX() << "; " << v.getY() << "; " << v.getZ() << ')';
+    return os;
+}
+
+istream &operator>>(istream &is, Vector3D &v) {
+    double _x, _y, _z;
+    is >> _x >> _y >> _z;
+    v.setX(_x);
+    v.setY(_y);
+    v.setZ(_z);
+    return is;
+}
--- Vector3D.h
+++ Vector3D.h
@@ -1,10 +1,51 @@
 #ifndef LINAL_VECTOR3D_H
 #define LINAL_VECTOR3D_H
 
+#include <iostream>
 
 class Vector3D {
+public:
+    Vector3D() : x(0), y(0), z(0) {}
 
+    Vector3D(double _x, double _y, double _z) : x(_x), y(_y), z(_z) {}
+
+    double getX() const;
+
+    double getY() const;
+
+    double getZ() const;
+
+    void setX(double _x);
+
+    void setY(double _y);
+
+    void setZ(double _z);
+
+    void rotateX(double angle); //Поворачивает вектор вокруг оси X (единичный вектор (1; 0; 0)) на угол angle, заданный в градусах
+
+    void rotateY(double angle); //Поворачивает веткор вокруг оси Y (единичный вектор (0; 1; 0)) на угол angle, заданный в градусах
+
+    void rotateZ(double angle); //Поворачивает веткор вокруг оси Z (единичный вектор (0; 0; 1)) на угол angle, заданный в градусах
+
+    bool operator==(const Vector3D &v2) const;
+
+    bool operator!=(const Vector3D &v2) const;
+
+    Vector3D operator+(const Vector3D &v2) const;
+
+    Vector3D operator-(const Vector3D &v2) const;
+
+    Vector3D operator*(const double a) const;
+
+    double operator*(const Vector3D &v2) const; //Скалярное произведение векторов
+
+private:
+    double x, y, z;
 };
+
+std::ostream &operator<<(std::ostream &os, const Vector3D &v);
+
+std::istream &operator>>(std::istream &is, Vector3D &v);
 
 
 #endif //LINAL_VECTOR3D_H
