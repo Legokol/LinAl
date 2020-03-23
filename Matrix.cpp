99357e01c1d01ec781f812d4067cdfed8a2e2e82
Nick <legokol3@gmail.com>
2020-03-23 16:32:33 +0300

    Matrix created

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,1 +1,54 @@
 #include "Matrix.h"
+#include <iostream>
+
+using namespace std;
+
+Matrix::Matrix(int _m, int _n) : m(_m), n(_n) {
+    mas = new double *[m];
+    for (int i = 0; i < m; i++)
+        mas[i] = new double[n];
+}
+
+Matrix::Matrix(int _m, int _n, double **a) : Matrix(_m, _n) {
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            mas[i][j] = a[i][j];
+}
+
+Matrix::~Matrix() {
+    delete[] mas;
+}
+
+int Matrix::getStr() const {
+    return m;
+}
+
+int Matrix::getCol() const {
+    return n;
+}
+
+double Matrix::get(int i, int j) const {
+    return mas[i][j];
+}
+
+void Matrix::set(int i, int j, double a) {
+    if (i >= m || j >= n) {
+        cout << "Can't set. Element out of range" << endl;
+        return;
+    }
+    mas[i][j] = a;
+}
+
+bool Matrix::operator==(const Matrix &m2) const {
+    if (m != m2.getStr() || n != m2.getCol())
+        return false;
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            if (mas[i][j] != m2.get(i, j))
+                return false;
+    return true;
+}
+
+bool Matrix::operator!=(const Matrix &m2) const {
+    return !(*this == m2);
+}
--- Matrix.h
+++ Matrix.h
@@ -1,10 +1,47 @@
 #ifndef LINAL_MATRIX_H
 #define LINAL_MATRIX_H
 
+#include <iostream>
+
+using namespace std;
 
 class Matrix {
+public:
+    Matrix(int _m, int _n);
 
+    Matrix(int _m, int _n, double **a);
+
+    ~Matrix();
+
+    int getStr() const;
+
+    int getCol() const;
+
+    double get(int i, int j) const;
+
+    void set(int i, int j, double a);
+
+    double det() const;
+
+    bool operator==(const Matrix &m2) const;
+
+    bool operator!=(const Matrix &m2) const;
+
+    Matrix operator+(const Matrix &m2) const;
+
+    Matrix operator-(const Matrix &m2) const;
+
+    Matrix operator*(double a) const;
+
+    Matrix operator*(const Matrix &m2) const;
+
+private:
+    const int m, n;
+    double **mas;
 };
 
+ostream &operator<<(ostream &os, const Matrix &M);
+
+istream &operator>>(istream &is, Matrix &M);
 
 #endif //LINAL_MATRIX_H
